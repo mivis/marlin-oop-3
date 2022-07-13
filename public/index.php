861a772a1820f8d7b86a8bfd8b4441c37cf39a9a
mivis <mivis@tut.by>
2022-07-13 23:30:07 +0300

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -2,8 +2,22 @@
 
 require ("../vendor/autoload.php");
 
+//
+// пока что не нашел способ как отображать темплейт без вызова его в главном роуте
+// если поместить темплейт в Home __construct, то не отрабатывает без вызова класса
+//
 if($_SERVER['REQUEST_URI'] == '/home') {
     include('../app/controllers/home.php');
+    $templates = new League\Plates\Engine('../app/views');    
+     echo $templates->render('home', [
+        'title' => 'Home.php controller',
+        'status' => '']);
 }
 
+//
+// обработчик форм /app/handlers/actions.php
+//
+if($_SERVER['REQUEST_URI'] == '/actions') {
+    include('../app/handlers/actions.php');
+}
 ?>
